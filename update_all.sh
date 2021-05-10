a3bd3f77b24e9750c7fe1e314812d286e78fa35f
Baal <baal@elk.finance>
2021-05-10 17:16:12 +0200

    Updated tokens

--- update_all.sh
+++ update_all.sh
@@ -2,6 +2,7 @@
 
 python update_list.py -o avax.tokenlist.json -c avax -u https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json --logos
 python update_list.py -o matic.tokenlist.json -c matic -u https://unpkg.com/quickswap-default-token-list@1.0.54/build/quickswap-default.tokenlist.json --logos
+#python update_list.py -o matic.tokenlist.json -c matic -u https://unpkg.com/@cometh-game/default-token-list@latest/build/comethswap-default.tokenlist.json --logos
 python update_list.py -o mumbai.tokenlist.json -c mumbai --logos
 python update_list.py -o fuji.tokenlist.json -c fuji --logos
 
