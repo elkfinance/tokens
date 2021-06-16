1efca972480eadde08f4f32a6dfe9b0c2729cc2e
Baal <baal@elk.finance>
2021-06-16 22:47:51 +0200

    Added HECO tokenlists

--- update_all.sh
+++ update_all.sh
@@ -4,10 +4,12 @@ python update_list.py -o avax.tokenlist.json -c avax -u https://raw.githubuserco
 python update_list.py -o matic.tokenlist.json -c matic -u https://unpkg.com/quickswap-default-token-list@1.0.54/build/quickswap-default.tokenlist.json --logos
 #python update_list.py -o matic.tokenlist.json -c matic -u https://unpkg.com/@cometh-game/default-token-list@latest/build/comethswap-default.tokenlist.json --logos
 python update_list.py -o ftm.tokenlist.json -c ftm --logos
+python update_list.py -o heco.tokenlist.json -c heco --logos
 python update_list.py -o mumbai.tokenlist.json -c mumbai --logos
 python update_list.py -o fuji.tokenlist.json -c fuji --logos
 python update_list.py -o ftmtest.tokenlist.json -c ftmtest --logos
+python update_list.py -o hecotest.tokenlist.json -c hecotest --logos
 
-for chain in avax matic ftm mumbai fuji ftmtest; do
+for chain in avax matic ftm heco mumbai fuji ftmtest hecotest; do
 	rsync -a logos/$chain/ logos/all/
 done
