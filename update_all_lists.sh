8a90fa23f2a0a73d2cca56e353b656813b97eee9
Baal <baal@elk.finance>
2022-01-24 21:40:57 -0500

    Bugfix

--- update_all_lists.sh
+++ update_all_lists.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for list in all top farms; do
-	python merge_lists.py -o $list.tokenlists.json -s symbols_$list.txt
+	python merge_lists.py -o $list.tokenlist.json -s symbols_$list.txt
 done
 
