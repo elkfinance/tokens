b8c679c71c14108fc771f06ce5ce85360c7c4a72
Baal <baal@elk.finance>
2022-01-24 21:23:31 -0500

    Scripts cleanup

---
+++ update_logos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+for chain in avax matic ftm heco bsc xdai fuse mumbai fuji ftmtest hecotest arbitrum elastos moonriver ethereum harmony hoo iotex kcc okex telos cronos; do
+	rsync -a logos/$chain/ logos/all/
+done
