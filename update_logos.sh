87ba80d51d9a369385ddbb234ed9b18835d00281
LtSnakePlissken <snake@elklabs.org>
2023-07-18 19:02:16 -0600

    Added new chains- Neon, Linea, Base, and Velas

--- update_logos.sh
+++ update_logos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for chain in avax bttc matic ftm heco bsc xdai fuse mumbai fuji ftmtest hecotest arbitrum elastos moonriver ethereum harmony hoo iotex kcc okex kava telos cronos optimism arthera_testnet astar bitgert metis neon_devnet pulse q wan; do
+for chain in avax bttc matic ftm heco bsc xdai fuse mumbai fuji ftmtest hecotest arbitrum elastos moonriver ethereum harmony hoo iotex kcc okex kava telos cronos optimism arthera_testnet astar bitgert metis neon_devnet neon q wan base velas linea; do
 	rsync -a logos/$chain/ logos/all/
 done
