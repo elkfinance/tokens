2a93d1324ca09ce26fd934c0371c205f88bed2ca
LtSnakePlissken <snake@elklabs.org>
2025-05-15 11:19:36 -0600

    Added cytonictest

--- fetch_new_lists.sh
+++ fetch_new_lists.sh
@@ -54,3 +54,4 @@ python update_list.py -o polygonzkevm.tokenlist.json -c polygonzkevm --logos
 python update_list.py -o berachain.tokenlist.json -c berachain --logos
 python update_list.py -o exptest.tokenlist.json -c exptest --logos
 python update_list.py -o sonic.tokenlist.json -c sonic --logos
+python update_list.py -o cytonictest.tokenlist.json -c cytonictest --logos
